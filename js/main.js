5a06d1f19a2a524dcfdcad475dd2d901a358c680
phamdat <tiendatt3009@gmail.com>
2023-01-09 01:41:04 +0700

    update

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function openAllCategory() {
+  const icon = document.querySelector(".category-head--icon");
+  const open = document.getElementsByClassName("open");
+  const categoryBody = document.querySelector(".category-body");
+  console.log(categoryBody)
+  if (open.length > 0) {
+    icon.classList.remove("open");
+    categoryBody.classList.remove("active");
+  } else {
+    icon.classList.add("open");
+    categoryBody.classList.add("active");
+  }
+}
