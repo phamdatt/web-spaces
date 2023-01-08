d99059a4281d3657be98fdce3412be8d07e182c3
phamdat <tiendatt3009@gmail.com>
2023-01-09 02:02:33 +0700

    update code

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function openAllCategory() {
   const icon = document.querySelector(".category-head--icon");
   const open = document.getElementsByClassName("open");
   const categoryBody = document.querySelector(".category-body");
-  console.log(categoryBody)
+  console.log(categoryBody);
   if (open.length > 0) {
     icon.classList.remove("open");
     categoryBody.classList.remove("active");
@@ -10,4 +10,13 @@ function openAllCategory() {
     icon.classList.add("open");
     categoryBody.classList.add("active");
   }
+}
+
+const elementModal = document.querySelector(".modal");
+
+function handleOpenModal() {
+  elementModal.classList.add("open");
+}
+function handleCloseModal() {
+  elementModal.classList.remove("open");
 }
